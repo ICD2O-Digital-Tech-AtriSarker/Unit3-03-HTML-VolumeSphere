35514361328f12700b9dd650aefadc06e3bb206b
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-09 14:48:45 +0000

    Main Commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICD2O Unit #X-YY
+# ICD2O Unit #3-03
 
 [![Mr Coxall's Super Linter](README.md/../../../workflows/Super%20Linter/badge.svg)](README.md/../../../actions)
 
 [![Deploy Pages](README.md/../../../workflows/Deploy%20Pages/badge.svg)](README.md/../../../actions)
 
-This site can be found at: [https://<OWNER>.github.io/<REPOSITORY>](https://<OWNER>.github.io/<REPOSITORY>)
+This site can be found at: [https://icd2o-digital-tech-atrisarker.github.io/Unit3-03-PHP-VolumeSphere](https://icd2o-digital-tech-atrisarker.github.io/Unit3-03-PHP-VolumeSphere)
--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict";
+// Script for Volume of a Right-Angled Triangular Pyramid
+
+// Elements
+// Inputs
+const radiusInput = document.getElementById('radiusInput');
+// Result Display
+const volumeResult = document.getElementById('volumeResult');
+// Calculate Button
+const calculateBtn = document.getElementById('CalculateBtn');
+
+// Code for calculating volume
+function Calculate() {
+
+  // Get Radius Input
+  let radius = Number(radiusInput.value);
+  
+  // Calculate Volume of the Sphere
+  let volume = (4/3) * Math.PI * ( radius ** 3 )
+
+  // Round up to 2 decimal places, if needed
+  if (volume % 1 != 0) {
+    volume = volume.toFixed(2);
+  }
+
+  // Display Result
+  volumeResult.innerHTML = "" + volume + "m<sup>3</sup>";
+  return;
+}
+
+// Connect button click to the Calculate() function, 
+calculateBtn.onclick = Calculate;
+
+// Initial Calculate() call
+Calculate();
